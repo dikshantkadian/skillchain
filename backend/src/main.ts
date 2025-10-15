c4d319777a93ad8fa8c7ba0091e4be55132b1fb4
dikshantkadian <152022960+dikshantkadian@users.noreply.github.com>
2025-10-15 22:01:20 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // --- REPLACE the simple app.enableCors() WITH THIS BLOCK ---
-  app.enableCors({
-    origin: '*', // Allow any origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
-  // ----------------------------------------------------------
-
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+{
+  "$schema": "https://openapi.vercel.sh/vercel.json",
+  "builds": [
+    {
+      "src": "src/main.ts",
+      "use": "@vercel/node"
+    }
+  ]
 }
-bootstrap();
+
