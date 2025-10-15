5a7dcc24a78791e6e1a7e77f8c7d637a93023ccd
dikshantkadian <152022960+dikshantkadian@users.noreply.github.com>
2025-10-15 22:03:38 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,20 @@
-{
-  "$schema": "https://openapi.vercel.sh/vercel.json",
-  "builds": [
-    {
-      "src": "src/main.ts",
-      "use": "@vercel/node"
-    }
-  ]
-}
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*', // Allow any origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
+
+  // Temporarily disable the global pipe to fix the crash
+  // app.useGlobalPipes(new ValidationPipe()); 
+
+  await app.listen(3000);
+}
+bootstrap();
